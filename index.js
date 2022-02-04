e062f0a3f32441f213461b943f9dffb332028b8f
Greg <gskril@gmail.com>
2022-02-03 23:01:16 -0500

    Fix ENS implementation

--- index.js
+++ index.js
@@ -16,35 +16,36 @@ app.listen(port, () => {
 app.get('/:address', async (req, res) => {
   const address = req.params.address
   
-  if (address.includes('.eth')) {
-    await axios
-      .get(`https://api.ensideas.com/ens/resolve/${address}`, {
-        setTimeout: 1000
-      })
-      .then(async (api) => {
-        let data = await api.data
-        let ens
-        if (data.name === null) {
-          ens = address.slice(0, 8)
-        } else if (data.address === null) {
-          // Check for invalid ENS name
-          return res.render('index.ejs', { error: 'Invalid ENS name' })
-        } else {
-          ens = data.name
-        }
-        res.render('donation.ejs', { address: address, ens: ens })
-      })
-      .catch(err => {
-        console.log(err.response)
+  if (address.length !== 42 && !address.endsWith('.eth')) {
+		return res.render('index.ejs', { error: 'Invalid wallet address' })
+  }
+
+  await axios
+    .get(`https://api.ensideas.com/ens/resolve/${address}`, {
+      setTimeout: 1000
+    })
+    .then(async (api) => {
+      let data = await api.data
+      let ens
+      if (data.name === null) {
+        ens = address.slice(0, 8)
+      } else if (data.address === null) {
+        // Check for invalid ENS name
+        return res.render('index.ejs', { error: 'Invalid ENS name' })
+      } else {
+        ens = data.name
+      }
+      res.render('donation.ejs', { address: address, ens: ens })
+    })
+    .catch(err => {
+      console.log('Error fetching data from ensideas api.', err.response)
+
+      try {
+        res.render('donation.ejs', { address: address, ens: address.slice(0, 8) })
+      } catch (error) {
         res.render('index.ejs', {
           error: 'Error resolving ENS name, please try with a full Ethereum address.'
         })
-      })
-  } else {
-    if (address.length !== 42 || !address.startsWith('0x')) {
-      res.render('index.ejs', { error: 'Invalid wallet address' })
-    } else {
-      res.render('donation.ejs', { address: address, ens: address.slice(0, 6) })
-    }
-  }
+      }
+    })
 })
