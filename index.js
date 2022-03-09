a8f7acafa34c1f0f0c466fa6ce3a3fc79d396857
Greg <gskril@gmail.com>
2022-03-09 16:17:38 -0500

    Optimize ENS record and transaction history lookup

--- index.js
+++ index.js
@@ -15,11 +15,17 @@ app.listen(port, () => {
 
 app.get('/:address', async (req, res) => {
   let address = req.params.address
+
+  // Special case for Ukraine
+  if (address === 'ukraine' || address === '0x165CD37b4C644C2921454429E7F9358d18A45e14') {
+    return res.render('donation.ejs', {
+      address: '0x165CD37b4C644C2921454429E7F9358d18A45e14',
+      ens: 'Ukraine',
+      avatar: null,
+    })
+  }
   
   if (address.length !== 42 && !address.endsWith('.eth')) {
-    if (address === 'ukraine') {
-      return res.render('donation.ejs', { address: '0x165CD37b4C644C2921454429E7F9358d18A45e14', ens: 'Ukraine' })
-    }
 		return res.render('index.ejs', { error: 'Invalid wallet address' })
   }
 
@@ -30,30 +36,28 @@ app.get('/:address', async (req, res) => {
     .then(async (api) => {
       let data = await api.data
       let ens
-      if (data.name === null) {
-        if (data.address === '0x165CD37b4C644C2921454429E7F9358d18A45e14') {
-          ens = 'Ukraine'
-        } else {
-          ens = address.slice(0, 8)
-        }
-      } else if (data.address === null) {
+      if (data.address === null) {
         // Check for invalid ENS name
         return res.render('index.ejs', { error: 'Invalid ENS name' })
       } else {
-        address = data.address
-        ens = data.name
-        avatar = data.avatar
+        res.render('donation.ejs', {
+          address: data.address,
+          ens: data.displayName,
+          avatar: data.avatar,
+        })
       }
-      res.render('donation.ejs', { address: address, ens: ens })
     })
     .catch(err => {
       console.log('Error fetching data from ensideas api.', err)
 
       try {
-        res.render('donation.ejs', { address: address, ens: address.slice(0, 8) })
+        res.render('donation.ejs', {
+          address: address,
+          ens: address.slice(0, 5) + '...' + address.slice(-4),
+        })
       } catch (error) {
         res.render('index.ejs', {
-          error: 'Error resolving ENS name, please try with a full Ethereum address.'
+          error: 'Error validating that address, please try again.'
         })
       }
     })
